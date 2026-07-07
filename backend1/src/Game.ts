f19788770e30d578fdcfd33a0aa825532ae75698
Eklavyasingh7 <codewitheklavya@gmail.com>
2026-07-07 23:06:44 +0530

    backend logic and chess.js framework added

--- backend1/src/Game.ts
+++ backend1/src/Game.ts
@@ -1,18 +1,67 @@
+import { Chess } from "chess.js";
 import type WebSocket from "ws";
+import { GAME_OVER, MOVE } from "./messages.js";
 
 export class Game{
 
-    private player1: WebSocket;
-    private player2: WebSocket;
-    private board: string;
-    private move: string[];
+    public player1: WebSocket;
+    public player2: WebSocket;
+    public board: Chess
     private startTime: Date;
 
     constructor(player1: WebSocket,player2: WebSocket){
         this.player1 = player1;
         this.player2 = player2;
-        this.board = "";
-        this.move = [];
+        this.board = new Chess();
         this.startTime = new Date();
+    }
+
+    makeMove(socket:WebSocket, move:{
+        from:string;
+        to: string;
+    }){
+            //validate the type of move using zod 
+            //validation 
+            // is this user move
+            //is this move valid
+
+            if(this.board.moves.length % 2 === 0 && socket !== this.player1){
+                return; 
+            }
+            if(this.board.moves.length % 2 === 1 && socket !== this.player2){
+                return;
+            }
+
+            try{
+                this.board.move(move);
+            }catch(e){
+                return;
+            }
+            //update the board
+            //push the move
+
+            //check if the game is over
+            if(this.board.isGameOver()){
+                //send the game over msg to both players
+                this.player1.emit(JSON.stringify({
+                    type: GAME_OVER,
+                    payload:{ 
+                        winner: this.board.turn() === "w" ? "black" : "white"
+                    }
+                }))
+                return;
+            }
+
+            if(this.board.moves.length % 2 === 0){
+                this.player2.emit(JSON.stringify({
+                    type: MOVE,
+                    payload: move
+                }))
+            }else{
+                this.player1.emit(JSON.stringify({
+                    type: MOVE,
+                    payload: move
+                }))
+            }        
     }
 }
--- backend1/src/GameManager.ts
+++ backend1/src/GameManager.ts
@@ -1,14 +1,16 @@
 import type WebSocket from "ws";
-import { INIT_GAME } from "./messages.js";
-import { searchForWorkspaceRoot } from "vite";
+import { INIT_GAME, MOVE } from "./messages.js";
+import { Game } from "./Game.js";
 
 export class GameManager{
     private games: Game[];
-    private pendingUser: WebSocket;
+    private pendingUser: WebSocket | null;
     private users: WebSocket[];
 
     constructor(){
         this.games = [];
+        this.pendingUser = null;
+        this.users = [];
     }
 
     addUser(socket:WebSocket){
@@ -28,11 +30,21 @@ export class GameManager{
             if(message.type === INIT_GAME){
                 if(this.pendingUser){
                     //start game
-                    
+                    const game = new Game(this.pendingUser,socket)
+                    this.games.push(game);
+                    this.pendingUser = null;
                 }else{
                     this.pendingUser = socket;
                 }
             }
+
+            if(message.type === MOVE){
+                const game = this.games.find(game=>game.player1 === socket || game.player2 === socket);
+                if(game){
+                    game.makeMove(message.move);
+                }
+            }
+
         }))
     }
 }
